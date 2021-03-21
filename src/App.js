dee990028b5d0a7164f383948ade1f866689870f
Softwarenginerig-S <softwareengineering-s@protonmail.com>
2021-03-21 16:04:18 +0100

    Added my Scrolldown Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import MyNavbar from "./components/my-navbar/mynavbar.component";
+import MyCarousel from "./components/myCarousel/my-carousel.component.jsx";
+import MyTitleMessage from "./components/title-message/title-message.component.jsx";
 import './App.css';
 
 const App = () => {
   return (
-    (<div>
+    <div>
       <MyNavbar />
-      My Portfolio</div>)
+      <MyCarousel />
+      <MyTitleMessage/>
+      </div>
   );
-}
+};
 
 export default App;
